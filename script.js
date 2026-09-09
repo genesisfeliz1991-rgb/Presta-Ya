b92336a323481cc1636903d7fde0e1e5df4676aa
genesisfeliz1991-rgb <genesisfeliz1991@gmail.com>
2026-09-09 09:46:31 -0400

    Remove Firebase initialization from script.js
    Removed Firebase SDK imports and initialization code.

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyC2SedBlK50Vv-ojsA3qnURG23gNdnwMTg",
@@ -15,7 +8,3 @@ const firebaseConfig = {
   appId: "1:55820924690:web:0a2fe35a7db7d6889065cb",
   measurementId: "G-2XM03ZDZ3J"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
